0e8683b0a46bd2813cf51c0c5e5e2fb7251914ce
mdabarik <mdabarik19@gmail.com>
2023-09-25 03:44:56 +0600

    localStorage implemented on cardDetails

--- src/components/CardDetails/CardDetails.jsx
+++ src/components/CardDetails/CardDetails.jsx
@@ -3,15 +3,31 @@ import { useLoaderData, useParams } from "react-router-dom";
 const CardDetails = () => {
     const cards = useLoaderData();
     const params = useParams();
-
     const card = cards.find(card => card.id == params.id);
+
+    const handleDonate = id => {
+        const storedCards = localStorage.getItem('donate');
+        if (!storedCards) {
+            const newCards = [];
+            newCards.push(id);
+            const cardsStringified = JSON.stringify(newCards);
+            localStorage.setItem('donate', cardsStringified);
+        } else {
+            const newCards = [...JSON.parse(storedCards)];
+            newCards.push(id);
+            const cardsStringified = JSON.stringify(newCards);
+            localStorage.setItem('donate', cardsStringified);
+        }
+    }
 
     return (
         <div>
             <div className="h-[600px] relative">
                 <img style={{ backgroundSize: 'cover', objectFit: 'cover' }} className="w-full h-[600px] rounded-lg" src={card.picture} />
                 <div style={{ background: 'rgba(11, 11, 11, 0.50)' }} className="absolute bottom-0 text-white w-full p-8 rounded-b-lg">
-                    <button style={{ background: card.color_text_button }} className="py-4 px-5 text-white font-semibold rounded-lg">Donate ${card.price}</button>
+                    <button 
+                        onClick={() => handleDonate(card.id)}
+                        style={{ background: card.color_text_button }} className="py-4 px-5 text-white font-semibold rounded-lg">Donate ${card.price}</button>
                 </div>
             </div>
             <h2 className="font-bold mt-5 text-black text-[30px] py-4">{card.title}</h2>
