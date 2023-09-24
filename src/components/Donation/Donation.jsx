e6a7ecf849e9801f6457ae786521977e66f7e050
mdabarik <mdabarik19@gmail.com>
2023-09-25 04:56:13 +0600

    donation page added

--- src/components/Donation/Donation.jsx
+++ src/components/Donation/Donation.jsx
@@ -1,8 +1,60 @@
+import { useEffect, useState } from "react";
+import DonationCard from "../DonationCard/DonationCard";
 
 const Donation = () => {
+
+    const [cardIds, setCardIds] = useState([]);
+    const [cards, setCards] = useState([]);
+    const [displayCards, setDisplayCards] = useState([]);
+    const [showAll, setShowAll] = useState(false);
+
+
+    useEffect(() => {
+        const storedCards = localStorage.getItem('donate');
+        if (storedCards !== null) {
+            const allCardsIds = JSON.parse(storedCards);
+            setCardIds(allCardsIds);
+        }
+    }, []);
+
+    useEffect(() => {
+        fetch('campaigns.json')
+            .then(res => res.json())
+            .then(data => setCards(data));
+    }, [cardIds])
+
+    useEffect(() => {
+        const display = cards.filter(card => cardIds.includes(card.id));
+        setDisplayCards(display);
+    }, [cardIds, cards])
+
+    console.log(displayCards);
+
     return (
-        <div>
-            Donation
+        <div className="mx-auto flex flex-col items-center justify-center mb-10">
+            <h2 className="text-3xl font-bold mb-8">Total Donations: {displayCards.length}</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-8">
+                
+                {   
+                    showAll ? 
+                    displayCards.map(card => <DonationCard key={card.id} card={card}></DonationCard>)
+                    :
+                    displayCards.slice(0,4).map(card => <DonationCard key={card.id} card={card}></DonationCard>)
+                }
+            </div>
+            {
+                displayCards.length <= 4 ? "" :
+
+                showAll 
+                ? 
+                <button 
+                    onClick={() => setShowAll(!showAll)}
+                    className="bg-[#009444] w-fit py-2 px-6 rounded-lg text-white">See Less</button>
+                :
+                <button 
+                    onClick={() => setShowAll(!showAll)}
+                    className="bg-[#009444] w-fit py-2 px-6 rounded-lg text-white">See All</button>
+            }
         </div>
     );
 };
---
+++ src/components/DonationCard/DonationCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const DonationCard = ({ card }) => {
+    return (
+        <div className='flex border border-blue-500 rounded-lg'>
+            <div className='w-[80%]'>
+                <img style={{'object-fit':'cover'}} className='h-full w-[100%]' src={card.picture} />
+            </div>
+            <div className='flex w-[100%] flex-col items-start justify-center px-6 py-8' style={{ backgroundColor: card.color_card_bg }}>
+                <h3 className='px-3 py-1 rounded-lg' style={{ backgroundColor: card.color_cat_bg }}>{card.category}</h3>
+                <h2 className='text-2xl mt-2 font-bold'>{card.title}</h2>
+                <p className='mt-2' style={{ color: card.color_text_button }}>${card.price}</p>
+                <button className='px-6 text-white py-2 mt-4 rounded-lg' style={{ backgroundColor: card.color_text_button }}>View Details</button>
+            </div>
+        </div>
+    );
+};
+
+export default DonationCard;
