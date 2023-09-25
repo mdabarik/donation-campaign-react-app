a49c7d528b112c2feff0f854738e0f52a2763a28
mdabarik <mdabarik19@gmail.com>
2023-09-25 07:07:52 +0600

    PieChart Added

---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,48 @@
+import { PieChart } from '@mui/x-charts/PieChart';
+import { useEffect, useState } from 'react';
+
+const Chart = () => {
+
+    const [myDonation, setMyDonation] = useState(0);
+
+    useEffect(() => {
+        const storedItem = JSON.parse(localStorage.getItem('donate'));
+        if (storedItem !== null) {
+            setMyDonation(storedItem.length);
+        }
+    }, [])
+
+    const data = [
+        { label: 'My Donation', value: myDonation, color: '#00C49F' },
+        { label: 'Total Donation', value: 12 - myDonation, color: 'red' },
+    ];
+
+    const sizing = {
+        margin: { right: 10 },
+        width: 500,
+        height: 500
+    };
+
+    const TOTAL = 12;
+
+    const getArcLabel = (params) => {
+        const percent = params.value / TOTAL;
+        return `${(percent * 100).toFixed(0)}%`;
+    };
+
+
+    return (
+        <PieChart
+            series={[
+                {
+                    outerRadius: 150,
+                    data,
+                    arcLabel: getArcLabel,
+                },
+            ]}
+            {...sizing}
+        />
+    )
+};
+
+export default Chart;
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,8 +1,9 @@
+import Chart from "../Chart/Chart";
 
 const Statistics = () => {
     return (
-        <div>
-            Statistics
+        <div className="flex justify-center items-center">
+            <Chart></Chart>
         </div>
     );
 };
