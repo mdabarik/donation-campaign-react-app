e2ef39cb31d3565c88f936b3d0c493dc240cec35
mdabarik <mdabarik19@gmail.com>
2023-09-25 06:19:38 +0600

    search functionality added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,12 @@
 
+import { useRef } from 'react';
 import './Banner.css'
 
-const Banner = () => {
+const Banner = ({handleFormSubmit}) => {
+    const myRef = useRef();
+    const handleSubmit = (e) => {
+        handleFormSubmit(e, myRef);
+    }
     return (
         <div className='h-[500px]'>
             <img className='absolute top-0 left-0 z-10 w-full h-[600px]' src="donation.jpg" />
@@ -9,10 +14,14 @@ const Banner = () => {
                 <div className='flex flex-col items-center justify-center h-[600px]'>
                     <h2 className='font-bold text-[40px]'>I Grow By Helping People In Need</h2>
                     <div className='mt-5'>
-                        <form className='flex border'>
-                            <input className='rounded-md w-[300px] bg-white text-[#0B0B0B66] text-[14px] outline-none border-[1px] border-[#DEDEDE] py-2 px-3' type="text" placeholder='Search here....' />
+
+                        <form 
+                            onSubmit={handleSubmit} 
+                            className='flex border'>
+                            <input ref={myRef} className='rounded-md w-[300px] bg-white text-[#0B0B0B66] text-[14px] outline-none border-[1px] border-[#DEDEDE] py-2 px-3' type="text" placeholder='Search here....' />
                             <input className='bg-[#FF444A] w-[100px] text-white text-[16px] py-2 px-3 rounded-r-lg ml-[-8px] border-[#FF444A]' type="submit" value="Search" />
                         </form>
+
                     </div>
                 </div>
             </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,35 @@
 import { useLoaderData } from "react-router-dom";
 import Cards from "../../components/Cards/Cards";
 import Banner from "../../components/Banner/Banner";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Home = () => {
-
+    const [displayCards, setDisplayCards] = useState([]);
     const cards = useLoaderData();
+
+    const handleFormSubmit = (e, myRef) => {
+        e.preventDefault();
+        const search_keyword = myRef.current.value.trim();
+        const filteredCards = cards.filter(card => card.category.toLowerCase().indexOf(search_keyword.toLowerCase()) != -1);
+        setDisplayCards(filteredCards)
+    }
+
+    useEffect(() => {
+        setDisplayCards(cards);
+    }, [])
 
     return (
         <div className="mb-8">
             <div>
-                <Banner></Banner>
+                <Banner handleFormSubmit={handleFormSubmit}></Banner>
             </div>
             <div>
-                <Cards cards={cards}></Cards>
+                {
+                    displayCards.length == 0 ?
+                        <h2 className="text-center">No result found, search with different keyword.</h2> :
+                        <Cards cards={displayCards}></Cards>
+                }
             </div>
         </div>
     );
