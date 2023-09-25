ec1f472bc425dc29fa29dfebe38ca79d34a79d3a
mdabarik <mdabarik19@gmail.com>
2023-09-25 14:16:56 +0600

    card image fluied

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -12,7 +12,7 @@ const Card = ({card}) => {
     return (
         <div className='rounded-lg' onClick={handleCardClick}>
             <div className='h-[180px]'>
-                <img className='w-full h-full rounded-t-lg' src={picture}/>
+                <img style={{objectFit: 'cover'}} className='w-full h-full rounded-t-lg' src={picture}/>
             </div>
             <div style={{background: color_card_bg, color: color_text_button}} className={`bg-[${color_card_bg}] rounded-b-lg p-4`}>
                 <p className="py-1 rounded-md mb-1 px-3 w-fit" style={{background: color_cat_bg, color:color_text_button}}>{category}</p>
