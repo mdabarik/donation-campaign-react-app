66c89658de0213bca141f758110afbcdd58fd7b2
mdabarik <mdabarik19@gmail.com>
2023-09-26 03:15:57 +0600

    added styles to the error page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,14 @@
+import { useNavigate } from "react-router-dom";
 
 const ErrorPage = () => {
+    const navigate = useNavigate();
+    const goBack = () => {
+        navigate(-1);
+    }
     return (
-        <div>
-            Error Page
+        <div className="flex flex-col justify-center items-center">
+            <h2 className="text-4xl text-center my-10">Opps! 404 Not Found.</h2>
+            <button className="btn btn-error" onClick={() => goBack()}>Go Back</button>
         </div>
     );
 };
